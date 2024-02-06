5bd499c1450baeb398ea8108cade2c73e72b916f
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-02-06 16:43:10 +0200

    add menu option

---
+++ src/main/java/org/example/service/console/MenuOption.java
@@ -0,0 +1,19 @@
+package org.example.service.console;
+
+public class MenuOption {
+
+    private int number;
+    private Runnable action;
+    private String nameAction;
+
+
+    public MenuOption(int number, Runnable action, String nameAction) {
+        this.number = number;
+        this.action = action;
+        this.nameAction = nameAction;
+    }
+
+    public void doAction() {
+        action.run();
+    }
+}
