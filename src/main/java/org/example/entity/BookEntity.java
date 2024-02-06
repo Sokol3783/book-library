08961a77906a749aad82a1008a636c874eea7f46
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-02-06 14:33:12 +0200

    add book entity

---
+++ src/main/java/org/example/entity/BookEntity.java
@@ -0,0 +1,51 @@
+package org.example.entity;
+
+public class BookEntity {
+
+  private long id;
+  private String name;
+  private String author;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof BookEntity)) {
+      return false;
+    }
+
+    BookEntity that = (BookEntity) o;
+    return id != that.id;
+  }
+
+  @Override
+  public int hashCode() {
+    return 37;
+  }
+
+}
