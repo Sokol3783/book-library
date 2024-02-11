f0573d00e06746d8e0a28f1ee10e054929867eea
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-02-11 15:11:19 +0200

    initiate scannner in entry point of application

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
+
+  private final static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
   }
