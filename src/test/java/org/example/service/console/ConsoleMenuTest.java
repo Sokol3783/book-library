89f43d563f86eae37d25234f9d49eb55ab82d741
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-02-11 16:38:11 +0200

    check print out welcome message is correct

--- src/test/java/org/example/service/console/ConsoleMenuTest.java
+++ src/test/java/org/example/service/console/ConsoleMenuTest.java
@@ -2,24 +2,53 @@ package org.example.service.console;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 import org.example.factory.MenuFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ConsoleMenuTest {
 
   private static MenuFactory factory = MenuFactory.getInstance(new Scanner(System.in));
+  private ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+  @BeforeEach
+  void setUpStream() {
+    System.setOut(new PrintStream(output));
+  }
+
+  @AfterEach
+  void cleanUpStreams() {
+    System.setOut(null);
+  }
 
   @ParameterizedTest
   @ValueSource(classes = {BaseConsoleMenu.class})
   void printWelcomeMessage(Class clazz) {
+      ConsoleMenu menu = factory.getMenu(clazz);
+      menu.printWelcomeMessage();
+
+      assertNotEquals(0, output.size());
+      assertEquals("WELCOME TO THE LIBRARY!\r\n", output.toString());
 
   }
 
   @ParameterizedTest
   @ValueSource(classes = {BaseConsoleMenu.class})
   void printMenu(Class clazz) {
+    ConsoleMenu menu = factory.getMenu(clazz);
+    menu.printMenu();
+
+    assertNotEquals(0, output.size());
+    String printedText = output.toString();
+
+    assertTrue(printedText.contains("exit"));
+    assertTrue(printedText.contains(""));
+
   }
 
   @ParameterizedTest
