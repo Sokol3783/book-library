cf03e3da596750064a00c9db2aa655df8ad2f1e1
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-02-14 17:32:09 +0200

    add repository default menu

---
+++ src/test/java/org/example/dao/BookRepositoryTest.java
@@ -0,0 +1,28 @@
+package org.example.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookRepositoryTest {
+
+  @Test
+  void findById() {
+  }
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void update() {
+  }
+
+  @Test
+  void deleteById() {
+  }
+
+  @Test
+  void findAll() {
+  }
+}
