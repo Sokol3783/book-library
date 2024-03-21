25993d2d5db740c5853e8082ea8850def5461717
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-03-21 14:01:46 +0200

    add test methods for base menu

---
+++ src/test/java/org/example/ui/BaseConsoleMenuTest.java
@@ -0,0 +1,54 @@
+package org.example.ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BaseConsoleMenuTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("Menu should print welcome message only once")
+    void shouldPrintWelcomeMessageOnlyOnce(){
+
+    }
+
+    @Test
+    @DisplayName("Menu should print three books after input '1'")
+    void shouldPrintThreeBooksAfterStart(){
+
+    }
+
+    @Test
+    @DisplayName("Menu should print three readers after input '2'")
+    void shouldPrintThreeReadersAfterStart(){
+
+    }
+
+    @Test
+    @DisplayName("Menu shouldn't crash after incorrect input")
+    void shouldNotFallDownAfterIncorrectInput(){
+
+    }
+
+    @Test
+    @DisplayName("Close menu after input 'exit'")
+    void shouldStopWorkingAfterInputExit(){
+
+    }
+
+    @Test
+    @DisplayName("Menu should print 'Goodbye!' after input 'exit'")
+    void shouldPrintGoodbyeAfterInputExit(){
+
+    }
+
+}
