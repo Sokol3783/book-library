dcca2267a1e6b3c4689b746c9005f70ec2bd4e32
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-03-21 13:50:41 +0200

    add test for successful build of app

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    @DisplayName("App should be build it ")
+    void testMain() {
+        assertTrue((true));
+    }
+}
