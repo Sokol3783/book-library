5d332c7741262ae18c398b980e312f4d726922f4
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-03-21 13:46:57 +0200

    remove old test and run menu from entrypoint

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.ui.BaseConsoleMenu;
+
 public class App {
 
   public static void main(String[] args) {
+      BaseConsoleMenu menu = new BaseConsoleMenu();
+      menu.run();
   }
 }
