5023a09740fe60967f22c213c2653f9afa412ac2
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-11 15:50:17 +0300

    implement reader repository

--- src/main/java/org/example/dao/ReaderRepository.java
+++ src/main/java/org/example/dao/ReaderRepository.java
@@ -1,21 +1,35 @@
 package org.example.dao;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
 import org.example.entity.Reader;
 
 public class ReaderRepository {
 
+  private long lastId = 0;
+  private final Set<Reader> readers= new TreeSet<>(Comparator.comparingLong(Reader::getId));
+
   public Optional<Reader> findById(long id){
-    return Optional.empty();
+    return readers.stream().filter(s -> s.getId() == id).findFirst();
   }
 
   public List<Reader> findAll(){
-    return List.of();
+    return List.copyOf(readers);
   }
 
  public Reader save(Reader reader){
-    return null;
+    if (reader.getId() == 0){
+      reader.setId(getNextId());
+    }
+    readers.add(reader);
+    return reader;
  }
+
+  private long getNextId() {
+    return ++lastId;
+  }
 
 }
