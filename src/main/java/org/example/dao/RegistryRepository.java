e27ff2a749c4254a1b843ec6b546afd84b45d1da
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-11 20:07:53 +0300

    think a bit about better implementation of class

--- src/main/java/org/example/dao/RegistryRepository.java
+++ src/main/java/org/example/dao/RegistryRepository.java
@@ -1,14 +1,24 @@
 package org.example.dao;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.example.entity.Book;
 import org.example.entity.Reader;
 
 public class RegistryRepository {
 
+  private final HashMap<Reader, Set<Book>> map = new HashMap<>();
+
   public boolean borrowBook(Book book, Reader reader) {
-    return false;
+    Set<Book> merge = map.merge(reader, Set.of(book),
+        (v1, v2) -> Stream.of(v1, v2).flatMap(Collection::stream).collect(
+            Collectors.toSet()));
+    return merge.contains(book);
   }
 
   public boolean returnBook(Book book, Reader reader) {
