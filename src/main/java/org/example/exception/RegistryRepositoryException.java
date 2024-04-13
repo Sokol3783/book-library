79f1ab5fae7befd7ab107823cd27805516617e56
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-13 20:42:25 +0300

    refactor exception

--- src/main/java/org/example/exception/RegistryRepositoryException.java
+++ src/main/java/org/example/exception/RegistryRepositoryException.java
@@ -1,20 +1,13 @@
 package org.example.exception;
 
-public class RegistryRepositoryException extends RuntimeException {
+
+public class RegistryRepositoryException extends Exception {
 
   public RegistryRepositoryException() {
   }
 
   public RegistryRepositoryException(String message) {
     super(message);
-  }
-
-  public RegistryRepositoryException(String message, Throwable cause) {
-    super(message, cause);
-  }
-
-  public RegistryRepositoryException(Throwable cause) {
-    super(cause);
   }
 
 }
