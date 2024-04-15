c15384638959a3a18086def77ceaa0e8bf55a57a
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-15 17:12:32 +0300

    create constructor of BookService, add write all setup and after test methods

--- src/main/java/org/example/services/BookService.java
+++ src/main/java/org/example/services/BookService.java
@@ -1,9 +1,16 @@
 package org.example.services;
 
 import java.util.List;
+import org.example.dao.BookRepository;
 import org.example.entity.Book;
 
 public class BookService {
+
+  private final BookRepository repository;
+
+  public BookService(BookRepository repository) {
+    this.repository = repository;
+  }
 
   public List<Book> getAllBooks() {
     return List.of();
--- src/test/java/org/example/services/BookServiceTest.java
+++ src/test/java/org/example/services/BookServiceTest.java
@@ -2,14 +2,42 @@ package org.example.services;
 
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.example.dao.BookRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BookServiceTest {
 
+  private static final ByteArrayOutputStream output = new ByteArrayOutputStream();
+  private static final ByteArrayOutputStream err = new ByteArrayOutputStream();
+
+  private static BookService service;
+  private static BookRepository repository;
+
+  @BeforeAll
+  static void beforeAll() {
+    System.setOut(new PrintStream(output));
+    System.setErr(new PrintStream(err));
+  }
+
+  @AfterAll
+  static void afterAll() {
+    System.setOut(System.out);
+    System.setErr(System.err);
+  }
+
   @BeforeEach
   void setUp() {
+    repository = mock(BookRepository.class);
+    service = new BookService(repository);
+    err.reset();
+    output.reset();
   }
 
   @Test
@@ -25,7 +53,6 @@ class BookServiceTest {
   @Test
   void shouldPrintThatTitleNotValid(){
     fail();
-
   }
 
   @Test
