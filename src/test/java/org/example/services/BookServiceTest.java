46df1eb5cccdbf5509ae0a9b898969b7af6372a0
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-15 19:23:16 +0300

    implement testcases and refactor after changes in BookService.class, all test are failed

--- src/test/java/org/example/services/BookServiceTest.java
+++ src/test/java/org/example/services/BookServiceTest.java
@@ -1,12 +1,21 @@
 package org.example.services;
 
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.example.util.Util.countRepeatedSubstrings;
+import static org.example.util.Util.getTestBooks;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 import org.example.dao.BookRepository;
+import org.example.entity.Book;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -41,23 +50,83 @@ class BookServiceTest {
   }
 
   @Test
-  void shouldCallRepositoryOnceWhenGetListOfBooks() {
-    fail();
+  void shouldCallRepositoryOnceWhenGetListOfEmptyBooksAndPrintHead() {
+    verify(repository, times(1)).findAll();
+    when(repository.findAll()).thenReturn(List.of());
+    service.printAllBooks();
+    assertTrue(output.toString().contains("Books in library:"));
+  }
+
+  @Test
+  void shouldCallRepositoryOnceWhenGetListOfBooksPrintEvery(){
+    verify(repository, times(1)).findAll();
+    List<Book> testBooks = getTestBooks();
+    when(repository.findAll()).thenReturn(testBooks);
+    service.printAllBooks();
+    String message = output.toString();
+    assertAll(() -> assertTrue(message.contains("Books in library:")),
+        () -> assertTrue(message.contains(testBooks.get(0).toString())),
+        () -> assertTrue(message.contains(testBooks.get(1).toString())),
+        () -> assertTrue(message.contains(testBooks.get(2).toString()))
+    );
+
+  }
+  @Test
+  void shouldPrintThatTitleContainInvalidSymbols(){
+    service.addNewBook("ta3#$\\author");
+    assertTrue(err.toString().contentEquals("Title contain invalid symbols"));
+  }
+
+  @Test
+  void shouldPrintThatAuthorContainsInvalidSymbols(){
+    service.addNewBook("valid\\a$l#uthor");
+    assertTrue(err.toString().contentEquals("Author contain invalid symbols"));
+  }
+
+  @Test
+  void shouldPrintErrThatSizeAuthorNotValid(){
+    service.addNewBook("valid\\asd");
+    service.addNewBook("valid\\$l#uyyyyyyyyyyyyesssssssssssssggthor");
+    assertEquals(2, countRepeatedSubstrings(err.toString(), "Invalid length of author"));
+    assertEquals(2, countRepeatedSubstrings(err.toString(), "Name should contain more than 5 char and less than 30 ones"));
+  }
+
+  @Test
+  void shoultPrintErrThatSizeTitleNotValid() {
+    service.addNewBook("valid\\asd");
+    service.addNewBook("valid\\$l#uyyyyyyyyyyyyesssssssssssssggthor");
+    assertEquals(2, countRepeatedSubstrings(err.toString(), "Invalid length of author"));
+    assertEquals(2, countRepeatedSubstrings(err.toString(),
+        "Name should contain more than 5 char and less than 30 ones"));
   }
 
   @Test
   void shouldCreateNewBookWithValidFieldsAndPrintMessage() {
-    fail();
+    Book book = new Book(1L, "Author 1", "Title 1");
+    when(repository.save(book)).thenReturn(book);
+    service.addNewBook(book.getName() + "\\" + book.getAuthor());
+    String message = output.toString();
+
+    assertAll(() -> assertTrue(message.contains("Book saved")),
+              () -> assertTrue(message.contains(book.toString())));
   }
 
   @Test
   void shouldPrintThatTitleNotValid(){
-    fail();
+    service.addNewBook("\\asisdas");
+    service.addNewBook("dsa\\a123sdas");
+    service.addNewBook("invailid#\\author");
+    service.addNewBook("bssssssssssssssssssssssssssssssssssssssssssssssssss555555555555555555555555555555555555555555555555555s\\sdfasdfas");
+    assertEquals(4, countRepeatedSubstrings(err.toString(), "Title not valid"));
   }
 
   @Test
   void shouldPrintThatAuthorNotValid(){
-    fail();
+    service.addNewBook("validtitle\\");
+    service.addNewBook("validtitle\\asda");
+    service.addNewBook("validtitle\\adas3##$43");
+    service.addNewBook("validtitle\\iiiiiiiiiiiiiiiijjkk55555555555555551");
+    assertEquals(4, countRepeatedSubstrings(err.toString(), "Author not valid"));
   }
 
 }
