428776b74e24b397aea592f2e7e8b1aa13accf29
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-15 17:03:55 +0300

    add test cases for BookService

---
+++ src/test/java/org/example/services/BookServiceTest.java
@@ -0,0 +1,36 @@
+package org.example.services;
+
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BookServiceTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void shouldCallRepositoryOnceWhenGetListOfBooks() {
+    fail();
+  }
+
+  @Test
+  void shouldCreateNewBookWithValidFieldsAndPrintMessage() {
+    fail();
+  }
+
+  @Test
+  void shouldPrintThatTitleNotValid(){
+    fail();
+
+  }
+
+  @Test
+  void shouldPrintThatAuthorNotValid(){
+    fail();
+  }
+
+}
