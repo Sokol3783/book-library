472c59d6171706baef8885f4d53c53f570d2e510
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-15 22:38:37 +0300

    implement all behavior of this class

--- src/main/java/org/example/services/BookService.java
+++ src/main/java/org/example/services/BookService.java
@@ -1,7 +1,10 @@
 package org.example.services;
 
-import java.util.regex.Pattern;
+import static org.example.validator.ValidatorUtil.invalidName;
+import static org.example.validator.ValidatorUtil.invalidTitle;
+
 import org.example.dao.BookRepository;
+import org.example.entity.Book;
 
 public class BookService {
 
@@ -17,38 +20,52 @@ public class BookService {
   }
 
   public void addNewBook(String input) {
-    if (isValid(input)) {
-
+    String[] arr = input.split("/");
+    if (isValidInput(arr)) {
+      Book save = repository.save(new Book(0l, arr[0], arr[1]));
+      System.out.println("Book saved:\n" + save.toString());
     }
   }
 
-  private boolean isValid (String input) {
-    String[] titleAndAuthor = input.split("/");
-    if (titleAndAuthor.length != 2){
-      System.out.println("Invalid input! Doesn't contain author.");
+  private boolean isValidInput(String[] titleAndAuthor) {
+    if (titleAndAuthor.length != 2) {
+      System.out.println("Invalid input! There is no / or to many / ");
       return false;
     }
-    if (alertNotValidTitle(titleAndAuthor[0])) return false;
-    if (alertNotValidAuthor(titleAndAuthor[1])) return false;
-    return true;
+    return alertNotValidTitle(titleAndAuthor[0]) && alertNotValidAuthor(titleAndAuthor[1]);
   }
 
   private boolean alertNotValidAuthor(String author) {
     int length = author.length();
+    String alert = "";
     if (length < 5 || length > 30) {
-      System.err.println("Invalid length of author");
-      System.err.println("Name should contain more than 5 char and less than 30 ones");
-      return false;
+      alert = "Invalid length of author\nName should contain more than 5 char and less than 30 ones";
     }
 
-    if (!Pattern.matches("^[a-zA-Z\\s'-]+$", author)) {
-      System.err.println("Author contain invalid symbols");
-      return false;
+    if (!invalidName(author)) {
+      alert += "Author must contain only letters, spaces, dashes, apostrophes!";
     }
-    return true;
+
+    if (alert.isEmpty()) return true;
+
+    System.err.println("Author is not valid\n" + alert);
+    return false;
   }
 
   private boolean alertNotValidTitle(String title) {
-    return true;
+    int length = title.length();
+    String alert = "";
+    if (length < 5 || length > 100) {
+      alert = "Invalid length of title\nTitle should contain more than 5 char and less than 100 ones";
+    }
+
+    if (!invalidTitle(title)) {
+      alert += "Title contains invalid symbols: |/\\#%=+*_><]";
+    }
+
+    if (alert.isEmpty()) return true;
+
+    System.err.println("Title is not valid\n" + alert);
+    return false;
   }
 }
