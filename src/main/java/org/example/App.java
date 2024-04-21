2f5b3d7e1805eb13cae9c975ca4621e11b08e219
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-21 16:21:09 +0300

    add init books and reader, also add whitespace for book toString

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,38 @@
 package org.example;
 
+import org.example.dao.BookRepository;
+import org.example.dao.ReaderRepository;
+import org.example.dao.RegistryRepository;
+import org.example.entity.Book;
+import org.example.entity.Reader;
+import org.example.services.BookService;
+import org.example.services.ReaderService;
+import org.example.services.RegistryService;
 import org.example.ui.AdvancedConsoleMenu;
 import org.example.ui.ConsoleMenu;
 
 public class App {
 
   public static void main(String[] args) {
-      ConsoleMenu menu = new AdvancedConsoleMenu();
+    BookRepository bookRepository = new BookRepository();
+    ReaderRepository readerRepository = new ReaderRepository();
+    generateInitialBooksList(bookRepository);
+    generateInitilReaderList(readerRepository);
+    ConsoleMenu menu = new AdvancedConsoleMenu(new BookService(bookRepository)
+                                              ,new ReaderService(readerRepository)
+                                              ,new RegistryService(new RegistryRepository()));
       menu.run();
+  }
+
+  private static void generateInitilReaderList(ReaderRepository readerRepository) {
+      readerRepository.save(new Reader(0, "Mike Douglas"));
+      readerRepository.save(new Reader(0, "Fedor Trybeckoi"));
+      readerRepository.save(new Reader(0, "Ivan Mazepa"));
+  }
+
+  private static void generateInitialBooksList(BookRepository bookRepository) {
+    bookRepository.save(new Book(0l, "The Dark Tower", "Steven King"));
+    bookRepository.save(new Book(0l, "The name of the Wind", "Patric Rotfuss"));
+    bookRepository.save(new Book(0l, "A Game of Thrones", "George Martin"));
   }
 }
