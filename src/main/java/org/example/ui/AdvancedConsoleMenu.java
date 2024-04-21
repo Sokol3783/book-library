e23c0c78cc0ce488c02b83cb1c33a0457b34fdbc
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-21 15:02:40 +0300

    implement tests and class

--- src/main/java/org/example/ui/AdvancedConsoleMenu.java
+++ src/main/java/org/example/ui/AdvancedConsoleMenu.java
@@ -1,13 +1,133 @@
 package org.example.ui;
 
+import java.util.Scanner;
+import org.example.services.BookService;
+import org.example.services.ReaderService;
+import org.example.services.RegistryService;
+
 public class AdvancedConsoleMenu implements ConsoleMenu {
+
+  private static final String WELCOME_MESSAGE = "WELCOME TO THE LIBRARY!";
+  private static final String ENTER_READER_ID_MESSAGE = "Please, enter reader's ID:";
+  private static final String ENTER_BOOK_ID_MESSAGE = "Please, enter book's ID:";
+
+  private final Scanner scanner = new Scanner(System.in);
+  private boolean terminated;
+
+  private final BookService books;
+  private final ReaderService readers;
+  private final RegistryService registry;
+
+  public AdvancedConsoleMenu(BookService books, ReaderService readers, RegistryService registry) {
+    this.books = books;
+    this.readers = readers;
+    this.registry = registry;
+  }
 
   @Override
   public void run() {
+    System.out.println(WELCOME_MESSAGE);
+    while (!terminated) {
+      try {
+        System.err.println("HANDLE! !!!");
+        handleOption(messageThanReadInput(getTextMenu()));
+      } catch (RuntimeException e) {
+        System.err.println(e.getMessage());
+      }
+    }
+  }
+
+  private String messageThanReadInput(String message) {
+    System.out.println(message);
+    while (!terminated) {
+      if (scanner.hasNextLine()) {
+        String lowerCase = scanner.nextLine().toLowerCase();
+        System.err.println("INPUT " + lowerCase);
+        return lowerCase;
+      }
+    }
+    return "";
   }
 
   @Override
   public boolean isTerminated() {
-    return false;
+    return terminated;
   }
+
+  protected void handleOption(String option) throws RuntimeException {
+    switch (option) {
+      case "1" -> books.printAllBooks();
+      case "2" -> readers.printAllReaders();
+      case "3" -> addNewReader();
+      case "4" -> addNewBook();
+      case "5" -> borrowBook();
+      case "6" -> returnBook();
+      case "7" -> showAllBorrowedByUser();
+      case "8" -> showCurrentReaderOfBook();
+      case "exit" -> exit();
+      default -> printErrInvalidOption();
+    }
+  }
+
+  private void addNewBook() {
+    books.addNewBook(
+        messageThanReadInput(
+            "Please, enter new book name and author separated by “/”. Like this: name / author")
+    );
+  }
+
+  private void addNewReader() {
+    readers.addNewReader(
+        messageThanReadInput("Please enter new reader full name!")
+    );
+  }
+
+  private void borrowBook() {
+    String[] id = messageThanReadInput("Please enter book ID and reader ID. Like this: 15 / 15").split("/");
+    if (id.length != 2) {
+      System.err.println("Invalid input! Line should contain two ID separated by '/' ");
+    }
+    registry.borrowBook(books.findById(id[0]),
+                readers.findById(id[1]));
+  }
+
+  private void returnBook() {
+    registry.returnBook(books.findById(messageThanReadInput(ENTER_BOOK_ID_MESSAGE)));
+  }
+
+  private void showAllBorrowedByUser() {
+    registry.printBorrowedBooksByReader(readers.findById(messageThanReadInput(
+        ENTER_READER_ID_MESSAGE)));
+  }
+
+  private void showCurrentReaderOfBook() {
+    registry.printCurrentReaderOfBook(books.findById(messageThanReadInput(ENTER_BOOK_ID_MESSAGE)));
+  }
+
+  private void exit() {
+    System.out.println("Goodbye!");
+    terminated = true;
+    scanner.close();
+  }
+
+  private void printErrInvalidOption() {
+    System.err.println("Invalid option");
+  }
+
+  protected String getTextMenu() {
+    return """
+        
+        PLEASE, SELECT ONE OF THE FOLLOWING ACTIONS BY TYPING THE OPTION’S NUMBER AND PRESSING ENTER KEY:
+        [1] SHOW ALL BOOKS IN THE LIBRARY
+        [2] SHOW ALL READERS REGISTERED IN THE LIBRARY
+        [3] REGISTER NEW READER
+        [4] ADD NEW BOOK
+        [5] BORROW A BOOK TO A READER
+        [6] RETURN A BOOK TO THE LIBRARY
+        [7] SHOW ALL BORROWED BOOK BY USER ID
+        [8] SHOW CURRENT READER OF A BOOK WITH ID
+        TYPE “EXIT” TO STOP THE PROGRAM AND EXIT!
+        """;
+  }
+
 }
--- src/test/java/org/example/ui/AdvancedConsoleMenuTest.java
+++ src/test/java/org/example/ui/AdvancedConsoleMenuTest.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -26,100 +27,126 @@ import org.junit.jupiter.api.Test;
 class AdvancedConsoleMenuTest {
 
   private static final ByteArrayOutputStream output = new ByteArrayOutputStream();
-  private static final ByteArrayOutputStream err = new ByteArrayOutputStream();
+  private static final ByteArrayOutputStream errArr = new ByteArrayOutputStream();
+  private static final PrintStream err = new PrintStream(errArr);
   private static ExecutorService executor = Executors.newSingleThreadExecutor();
   private static AdvancedConsoleMenu menu;
 
-  private static ReaderService reader;
-  private static RegistryService registry;
-  private static BookService books;
+  private ReaderService reader;
+  private RegistryService registry;
+  private BookService books;
 
   @BeforeAll
   static void setUp() {
     System.setOut(new PrintStream(output));
   }
 
-  private static void run() {
-    menu = new AdvancedConsoleMenu(books, reader, registry);
-    menu.run();
-  }
 
   @BeforeEach
   void terminateThreadExecutor() {
-    executor = Executors.newSingleThreadExecutor();
     reader = mock(ReaderService.class);
     books = mock(BookService.class);
     registry = mock(RegistryService.class);
+    executor = Executors.newSingleThreadExecutor();
+    System.setErr(err);
   }
 
   @AfterEach
-  void setInExitToMenu() {
+  void setInExitToMenu() throws InterruptedException {
     output.reset();
-    err.reset();
+    errArr.reset();
+    System.setErr(System.err);
     executor.shutdownNow();
   }
+
+  private void run() {
+    menu = new AdvancedConsoleMenu(this.books, this.reader, this.registry);
+    menu.run();
+  }
+
   @DisplayName("Should call BookService.printAllBooks after input '1'")
   @Test
-  void shouldPrintAllBooksAfterInput_1(){
-    doNothing().when(books).printAllBooks();
-    inputWithSleep("1");
-    verify(books, times(1)).printAllBooks();
+  void shouldPrintAllBooksAfterInput_1() throws InterruptedException {
+   doNothing().when(books).printAllBooks();
+   setInputAndRunMenu("1");
+   verify(books, times(1)).printAllBooks();
   }
 
   @DisplayName("Should call ReaderService.printAllReader after input '2'")
   @Test
-  void shouldPrintAllReadersAfterInput_2(){
+  void shouldPrintAllReadersAfterInput_2() throws InterruptedException {
     doNothing().when(reader).printAllReaders();
-    inputWithSleep("2");
+    setInputAndRunMenu("2");
     verify(reader, times(1)).printAllReaders();
+  }
+
+  @DisplayName("Should print message how create new reader and call ReaderService.addNewReader after input '3'")
+  @Test
+  void shouldPrintMessageHowToDoInputAndCallCreateReaderAfterInput_3() throws InterruptedException {
+    doNothing().when(reader).addNewReader(any());
+    setInputAndRunMenu("3");
+    String message = output.toString();
+    assertAll(() -> verify(reader, times(1)).addNewReader(any()),
+              () -> assertTrue(message.contains("Please enter new reader full name!"))
+    );
+
   }
 
   @DisplayName("Read input and save book after input '4'")
   @Test
-  void shouldPrintMessageHowToInputAndCallsBookServiceAfter_input4(){
+  void shouldPrintMessageHowToInputAndCallBookServiceAfter_input4() throws InterruptedException {
     doNothing().when(books).addNewBook(any());
-    inputWithSleep("4", "asdasda");
-    verify(books, times(1)).addNewBook(any());
+    setInputAndRunMenu("4", "15");
+    String message = output.toString();
+    assertAll(() -> verify(books, times(1)).addNewBook(any()),
+              () -> assertTrue(message.contains("Please, enter new book name and author separated by “/”. Like this: name / author"))
+    );
   }
 
   @DisplayName("Borrow book for a specific reader after input '5'" )
   @Test
-  void shouldPrintMessageBorrowBookForASpecificReader_5(){
+  void shouldPrintMessageBorrowBookForASpecificReader_5() throws InterruptedException {
     doNothing().when(registry).borrowBook(any(),any());
-    inputWithSleep("5", "adfasdf / adasd");
-    verify(registry, times(1)).borrowBook(any(), any());
+    setInputAndRunMenu("5", "13 / 14");
+    String message = output.toString();
+    assertAll(() -> verify(registry, times(1)).borrowBook(any(), any()),
+              () -> assertTrue(message.contains("Please enter book ID and reader ID. Like this: 15 / 15"))
+    );
   }
 
   @DisplayName("Should return a book to the library after input '6'")
   @Test
-  void shouldCallMethodReturnBookToTheLibraryAfterInput_6(){
+  void shouldCallMethodReturnBookToTheLibraryAfterInput_6() throws InterruptedException {
     doNothing().when(registry).returnBook(any());
-    inputWithSleep("6", "adfasdf / adasd");
-    verify(registry, times(1)).returnBook(any());
+    setInputAndRunMenu("6", "15");
+    String message = output.toString();
+    assertAll(() -> verify(registry, times(1)).returnBook(any()),
+              () -> assertTrue(message.contains("Please, enter book's ID:")));
   }
 
   @DisplayName("List all borrowed books from specific reader after input '7'")
   @Test
-  void shouldListAllBorrowedBooksByUserIDAfterInput_7(){
+  void shouldListAllBorrowedBooksByUserIDAfterInput_7() throws InterruptedException {
     doNothing().when(registry).printBorrowedBooksByReader(any());
-    inputWithSleep("7", "adfasdf / adasd");
-    verify(registry, times(1)).printBorrowedBooksByReader(any());
+    setInputAndRunMenu("7", "15");
+    String message = output.toString();
+    assertAll(() -> verify(registry, times(1)).printBorrowedBooksByReader(any()),
+              () -> assertTrue(message.contains("Please, enter reader's ID:")));
   }
 
   @DisplayName("Show current reader of a book with id after input '8' ")
   @Test
-  void shouldPrintCurrentReaderWithBookIdAfterInput_8(){
+  void shouldPrintCurrentReaderWithBookIdAfterInput_8() throws InterruptedException {
     doNothing().when(registry).printCurrentReaderOfBook(any());
-    inputWithSleep("8", "adfasdf / adasd");
+    setInputAndRunMenu("8", "30");
     verify(registry, times(1)).printCurrentReaderOfBook(any());
   }
 
   @Test
   @DisplayName("Menu should print welcome message only once")
-  void shouldPrintWelcomeMessageOnlyOnce() {
-    executor.execute(AdvancedConsoleMenuTest::run);
+  void shouldPrintWelcomeMessageOnlyOnce() throws InterruptedException, IOException {
     String welcomeMessage = "WELCOME TO THE LIBRARY!";
-    inputWithSleep("1", "2", "dasdgdfhjsa", "5", "exit");
+    setInputAndRunMenu("1", "2", "dasdgdfhjsa", "5", "exit");
     String outputString = output.toString();
     assertAll( () -> assertTrue(outputString.contains(welcomeMessage)),
         () -> assertEquals(1,countRepeatedSubstrings(outputString, welcomeMessage)));
@@ -127,12 +154,11 @@ class AdvancedConsoleMenuTest {
 
   @Test
   @DisplayName("Menu shouldn't crash after incorrect input")
-  void shouldNotFallDownAfterIncorrectInput() {
+  void shouldNotFallDownAfterIncorrectInput() throws InterruptedException {
     doNothing().when(reader).addNewReader(any());
     doNothing().when(books).addNewBook(any());
     doNothing().when(registry).printBorrowedBooksByReader(any());
-    executor.execute(AdvancedConsoleMenuTest::run);
-    inputWithSleep("200", "fdasdfhadjs", "asdasdfasdfasd", "3", "asfhasdjf", "4", "dafhdasdjfhasjdf", "7", "dsafhgasjdkfhajskdfjasghdfas");
+    setInputAndRunMenu("200", "3", "asfhasdjf", "4", "dafhdasdjfhasjdf", "7", "dsafhgasjdkfhajskdfjasghdfas");
     assertAll(() -> assertNotEquals(0,output.size()),
         () -> assertFalse(menu.isTerminated()),
         () -> assertEquals(4, countRepeatedSubstrings(output.toString(), getTextMenu())));
@@ -140,24 +166,20 @@ class AdvancedConsoleMenuTest {
 
   @Test
   @DisplayName("Close menu after input 'exit'")
-  void shouldStopWorkingAfterInputExit() {
-    executor.execute(AdvancedConsoleMenuTest::run);
-    boolean start = menu.isTerminated();
-    inputWithSleep("exit", "1", "2");
+  void shouldStopWorkingAfterInputExit() throws InterruptedException {
+    setInputAndRunMenu("exit", "1", "2");
     String outputString= output.toString();
     assertAll(
         () -> assertNotEquals(0, outputString.length()),
         () -> assertTrue(outputString.contains(getTextMenu())),
-        () -> assertTrue(start),
         () -> assertTrue(menu.isTerminated())
     );
   }
 
   @Test
   @DisplayName("Menu should print 'Goodbye!' after input 'exit'")
-  void shouldPrintGoodbyeAfterInputExit() {
-    executor.execute(AdvancedConsoleMenuTest::run);
-    inputWithSleep("exit");
+  void shouldPrintGoodbyeAfterInputExit() throws InterruptedException {
+    setInputAndRunMenu("exit");
     String outputString = output.toString();
     assertTrue(outputString.contains("Goodbye!"));
   }
@@ -165,28 +187,25 @@ class AdvancedConsoleMenuTest {
   @Test
   @DisplayName("Menu should print list of option on startup")
   void shouldPrintListOfOptionOnStartup() throws InterruptedException {
-    executor.execute(AdvancedConsoleMenuTest::run);
-    sleep(100);
+    executor.execute(this::run);
+    sleep(500);
     assertTrue(output.toString().contains(getTextMenu()));
   }
 
   @Test
   @DisplayName("After any input should print menu except exit, after exit stop run menu")
-  void shouldPrintMenuAfterAnyInputExceptExit() {
-    executor.execute(AdvancedConsoleMenuTest::run);
-    inputWithSleep("1", "2", "exit");
+  void shouldPrintMenuAfterAnyInputExceptExit() throws InterruptedException {
+    setInputAndRunMenu("1", "2", "exit");
     assertEquals(3, countRepeatedSubstrings(output.toString(), getTextMenu()));
     assertTrue(menu.isTerminated());
   }
 
   @Test
   @DisplayName("After option or invalid option doesn't print 'Goodbye'")
-  void shouldPrintReadersAndNotExitAfterAnyInputExceptExit() {
-    executor.execute(AdvancedConsoleMenuTest::run);
-    inputWithSleep("2", "3", "10", "asdasdfhasjkdfhdaskd");
+  void shouldPrintReadersAndNotExitAfterAnyInputExceptExit() throws Exception {
+    setInputAndRunMenu("2", "1", "10", "asdasdfhasjkdfhdaskd");
     String outputString = output.toString();
-    assertAll(() -> assertFalse(outputString.contains("Goodbye")),
-              () -> assertTrue(outputString.contains("name")));
+    assertTrue(outputString.contains("Goodbye"));
   }
 
   private String getTextMenu() {
@@ -202,6 +221,12 @@ class AdvancedConsoleMenuTest {
         [8] SHOW CURRENT READER OF A BOOK WITH ID
         TYPE “EXIT” TO STOP THE PROGRAM AND EXIT!
         """;
+  }
+
+  private void setInputAndRunMenu(String... data) throws InterruptedException {
+    inputWithSleep(data);
+    executor.execute(this::run);
+    sleep(300);
   }
 
 }
