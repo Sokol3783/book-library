8778d123aa3a6039fcea4d5ddc263a87d684ecd4
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-28 19:50:36 +0300

    remove class from name

---
+++ src/main/java/org/example/exception/ConsoleValidationException.java
@@ -0,0 +1,12 @@
+package org.example.exception;
+
+public class ConsoleValidationException extends RuntimeException{
+
+  public ConsoleValidationException() {
+    super();
+  }
+
+  public ConsoleValidationException(String message) {
+    super(message);
+  }
+}
