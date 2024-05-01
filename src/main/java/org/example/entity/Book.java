0265bf8a62a517b1f4210183a24ee7bb1a7f7608
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-05-01 21:37:39 +0300

    add constructor without id

--- src/main/java/org/example/entity/Book.java
+++ src/main/java/org/example/entity/Book.java
@@ -1,7 +1,5 @@
 package org.example.entity;
 
-import java.util.Objects;
-
 public class Book {
     private long id;
     private String name;
@@ -9,6 +7,11 @@ public class Book {
 
     public Book(long id, String name, String author) {
         this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public Book(String name, String author){
         this.name = name;
         this.author = author;
     }
--- src/main/java/org/example/entity/Reader.java
+++ src/main/java/org/example/entity/Reader.java
@@ -1,7 +1,5 @@
 package org.example.entity;
 
-import java.util.Objects;
-
 public class Reader {
 
     private long id;
@@ -9,6 +7,10 @@ public class Reader {
 
     public Reader(long id, String name) {
         this.id = id;
+        this.name = name;
+    }
+
+    public Reader(String name){
         this.name = name;
     }
 
