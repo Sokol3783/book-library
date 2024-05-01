afa5d15ac01862e0a557fa9e2f3dd08e0730bdf7
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-05-01 22:07:49 +0300

    remove repeat of methods in ValidatorUtil

--- src/main/java/org/example/validator/ValidatorUtil.java
+++ src/main/java/org/example/validator/ValidatorUtil.java
@@ -24,15 +24,15 @@ public class ValidatorUtil {
 
   public static void validateInputOfNewBook(String input) {
     StringBuilder message = new StringBuilder();
+    String[] titleAndAuthor;
     if (inputContainsSingleSlash(input)){
-      String[] titleAndAuthor = input.split("/");
-      validateTitle(titleAndAuthor[0].strip(), message);
-      validateNameOrAuthor(titleAndAuthor[1].strip(), "Author", message);
-    } else {
+      titleAndAuthor = input.split("/");
+   } else {
       message.append("\nLine should contain singe dash");
-      validateTitle(input, message);
-      validateNameOrAuthor("", "Author", message);
+      titleAndAuthor = new String[]{input, "\\"};
     }
+    validateTitle(titleAndAuthor[0].strip(), message);
+    validateNameOrAuthor(titleAndAuthor[1].strip(), "Author", message);
 
     if(!message.isEmpty()) throw new ConsoleValidationException(message.toString().strip());
 
