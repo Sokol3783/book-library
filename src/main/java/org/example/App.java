8192c3b7f51c5a3a0b4d14aca6d712fa328ad42a
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-05-01 21:38:13 +0300

    remove from App all layers except UI

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,12 @@
 package org.example;
 
-import org.example.dao.BookRepository;
-import org.example.dao.ReaderRepository;
-import org.example.dao.RegistryRepository;
-import org.example.services.BookService;
-import org.example.services.ReaderService;
-import org.example.services.RegistryService;
 import org.example.ui.ConsoleMenu;
 
 public class App {
 
   public static void main(String[] args) {
-    BookService bookService = new BookService(new BookRepository());
-    ReaderService readerService = new ReaderService(new ReaderRepository());
-    ConsoleMenu menu = new ConsoleMenu(bookService, readerService,
-        new RegistryService(new RegistryRepository(), bookService, readerService));
-    menu.run();
+   ConsoleMenu menu = new ConsoleMenu();
+   menu.run();
   }
 
 }
