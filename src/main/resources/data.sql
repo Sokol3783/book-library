e1ed6e8d600271bcefafd53ea8d38cb2dbe55393
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-05-03 23:24:34 +0300

    add data insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO book(title,author) VALUES
+                ('The Dark Tower', 'Steven King'),
+                ('The name of the Wind', 'Patric Rotfuss'),
+                ('A Game of Thrones', 'George Martin');
