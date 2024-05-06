367047aad2318419ea941fbb75e2efbef6958258
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-05-06 18:01:55 +0300

    add initial methods for creating database and tables

--- src/main/java/org/example/dao/DBUtil.java
+++ src/main/java/org/example/dao/DBUtil.java
@@ -1,13 +1,19 @@
 package org.example.dao;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 import javax.sql.DataSource;
 import org.postgresql.ds.PGSimpleDataSource;
 
 public class DBUtil {
 
-  private final static String URL = "jdbc:postgresql://localhost/library";
+  private final static String URL = "jdbc:postgresql://localhost:5432/library";
   private final static String LOGIN = "sam";
   private final static String PASSWORD = "password";
 
@@ -25,6 +31,42 @@ public class DBUtil {
 
   static Connection getConnection() throws SQLException {
     return dataSource.getConnection();
+  }
+
+  public static void initDatabase() {
+    try (Connection connection = getConnection()) {
+      createTables(connection);
+      insertInitialValue(connection);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void insertInitialValue(Connection connection) throws SQLException {
+    String data = readFromResources("data.sql");
+    PreparedStatement statement = connection.prepareStatement(data);
+    if (statement.execute()) {
+      statement.close();
+    }
+  }
+
+  private static void createTables(Connection connection) throws SQLException {
+    String schema = readFromResources("schema.sql");
+    PreparedStatement statement = connection.prepareStatement(schema);
+    if (statement.execute()) {
+      statement.close();
+    }
+  }
+
+  private static String readFromResources(String resourceName) {
+    String file = DBUtil.class.getResource(resourceName).getFile();
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      return reader.lines().collect(Collectors.joining("\n"));
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
