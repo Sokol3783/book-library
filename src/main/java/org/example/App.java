a713c4d9b7e8313749e92f0419b5012c1a555d48
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-05-06 18:02:20 +0300

    add initialization of DB to main class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import org.example.dao.DBUtil;
 import org.example.ui.ConsoleMenu;
 
 public class App {
 
   public static void main(String[] args) {
+    DBUtil.initDatabase();
     ConsoleMenu menu = new ConsoleMenu();
     menu.run();
   }
