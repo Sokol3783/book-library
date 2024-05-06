0c4ae63df0c02a271590f249bb883b19122b9184
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-05-06 17:22:38 +0300

    refactor book repository for working with DB

--- src/main/java/org/example/dao/BookRepository.java
+++ src/main/java/org/example/dao/BookRepository.java
@@ -1,43 +1,78 @@
 package org.example.dao;
 
-import java.util.Comparator;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.concurrent.atomic.AtomicLong;
 import org.example.entity.Book;
 
 public class BookRepository {
 
-  private final AtomicLong ID_GENERATOR = new AtomicLong(0);
-
-  private final Set<Book> books;
 
   public BookRepository() {
-    books = new TreeSet<>(Comparator.comparingLong(Book::getId));
-    Book book = new Book("The Dark Tower", "Steven King");
-    book.setId(ID_GENERATOR.incrementAndGet());
-    books.add(book);
-    book = new Book( "The name of the Wind", "Patric Rotfuss");
-    book.setId(ID_GENERATOR.incrementAndGet());
-    books.add(book);
-    book = new Book( "A Game of Thrones", "George Martin");
-    book.setId(ID_GENERATOR.incrementAndGet());
-    books.add(book);
   }
 
   public Optional<Book> findById(long id) {
-    return books.stream().filter(s -> s.getId() == id).findFirst();
+    try (Connection connection = DBUtil.getConnection()) {
+      PreparedStatement statement = connection.prepareStatement(
+          "SELECT id, name, title FROM book WHERE id = ?");
+      statement.setLong(1, id);
+      ResultSet resultSet = statement.executeQuery();
+      if (resultSet.next()) {
+        Book book = parseBook(resultSet);
+        return Optional.of(book);
+      }
+      return Optional.empty();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public List<Book> findAll() {
+    List<Book> books = new ArrayList<>();
+    try (Connection connection = DBUtil.getConnection()) {
+      PreparedStatement statement = connection.prepareStatement("SELECT id, name, title FROM book");
+      ResultSet resultSet = statement.executeQuery();
+      while (resultSet.next()) {
+        Book book = parseBook(resultSet);
+        books.add(book);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
     return List.copyOf(books);
   }
 
   public Book save(Book book) {
-    book.setId(ID_GENERATOR.incrementAndGet());
-    books.add(book);
+    try (Connection connection = DBUtil.getConnection()) {
+      PreparedStatement statement = connection.prepareStatement(
+          "INSERT INTO book(title, author) VALUES (?,?)",
+          Statement.RETURN_GENERATED_KEYS);
+      statement.setString(1, book.getName());
+      statement.setString(2, book.getAuthor());
+      statement.execute();
+      ResultSet generatedKeys = statement.getGeneratedKeys();
+      if (generatedKeys.next()) {
+        book.setId(generatedKeys.getInt(1));
+        return book;
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+    throw new RuntimeException("Book doesn't save");
+  }
+
+  private Book parseBook(ResultSet resultSet) throws SQLException {
+    Book book = new Book();
+    book.setId(resultSet.getLong("id"));
+    book.setAuthor(resultSet.getString("author"));
+    book.setName(resultSet.getString("title"));
     return book;
   }
+
 }
