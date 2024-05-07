bd8a88d019e1e75757f84c241df71a4c983bda8c
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-05-07 19:47:26 +0300

    add DAOException for messaging problems with SQL queries or DB

---
+++ src/main/java/org/example/exception/DAOException.java
@@ -0,0 +1,11 @@
+package org.example.exception;
+
+public class DAOException extends Throwable {
+
+  public DAOException() {
+  }
+
+  public DAOException(String message) {
+    super(message);
+  }
+}
