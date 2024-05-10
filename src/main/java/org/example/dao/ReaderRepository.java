d6a58284b8b6a1ac90de4e3d82bfe7dff0dc354e
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-05-10 15:10:52 +0300

    remove all implementation which doesn't work with DB

--- src/main/java/org/example/dao/ReaderRepository.java
+++ src/main/java/org/example/dao/ReaderRepository.java
@@ -1,43 +1,24 @@
 package org.example.dao;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.concurrent.atomic.AtomicLong;
 import org.example.entity.Reader;
 
 public class ReaderRepository {
 
-  private final AtomicLong ID_GENERATOR = new AtomicLong(0);
-  private final Set<Reader> readers;
-
   public ReaderRepository() {
-    this.readers = new TreeSet<>(Comparator.comparingLong(Reader::getId));
-    Reader reader = new Reader("Mike Douglas");
-    reader.setId(ID_GENERATOR.incrementAndGet());
-    readers.add(reader);
-    reader = new Reader("Fedor Trybeckoi");
-    reader.setId(ID_GENERATOR.incrementAndGet());
-    readers.add(reader);
-    reader = new Reader("Ivan Mazepa");
-    reader.setId(ID_GENERATOR.incrementAndGet());
-    readers.add(reader);
   }
 
   public Optional<Reader> findById(long id) {
-    return readers.stream().filter(s -> s.getId() == id).findFirst();
+    return null;
   }
 
   public List<Reader> findAll() {
-    return List.copyOf(readers);
+    return null;
   }
 
   public Reader save(Reader reader) {
-    reader.setId(ID_GENERATOR.incrementAndGet());
-    readers.add(reader);
-    return reader;
+    return null;
   }
 
 }
--- src/main/java/org/example/dao/RegistryRepository.java
+++ src/main/java/org/example/dao/RegistryRepository.java
@@ -1,55 +1,46 @@
 package org.example.dao;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.Set;
 import org.example.entity.Book;
 import org.example.entity.Reader;
-import org.example.exception.RegistryRepositoryException;
 
 public class RegistryRepository {
 
-  private final Map<Reader, Set<Book>> map;
-
   public RegistryRepository() {
-    map = new HashMap<>();
   }
 
   public void borrowBook(Book book, Reader reader) {
+   /*
     if (isBorrowedBook(book)) {
       throw new RegistryRepositoryException("Book is already borrowed! You can't borrow it");
     }
     map.computeIfAbsent(reader, k -> new HashSet<>()).add(book);
+    */
   }
 
   private boolean isBorrowedBook(Book book) {
+    return false;
+    /*
     return map.entrySet().stream().anyMatch(s -> s.getValue().contains(book));
+     */
   }
 
   public void returnBook(Book book) {
+/*
     map.values().stream()
         .filter(s -> s.remove(book))
         .findAny().
         orElseThrow(() -> new RegistryRepositoryException("This book anybody doesn't borrow!"));
+ */
   }
 
   public Optional<Reader> getReaderOfBook(Book book) {
-    return map.entrySet().stream().
-        filter(s -> s.getValue().contains(book)).
-        map(Entry::getKey).
-        findFirst();
+    return Optional.empty();
   }
 
   public List<Book> getListBorrowedBooksOfReader(Reader reader) {
-    return Optional.ofNullable(map.get(reader))
-        .map(List::copyOf)
-        .orElse(Collections.emptyList());
+    return List.of();
   }
-
 
 }
