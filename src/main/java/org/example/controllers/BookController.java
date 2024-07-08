7b189aae8bcc408f4caae2dd37092cd6560a5919
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-07-09 00:03:36 +0300

    implement all methods, all work except validation with regexp

--- src/main/java/org/example/controllers/BookController.java
+++ src/main/java/org/example/controllers/BookController.java
@@ -1,42 +1,58 @@
 package org.example.controllers;
 
 
-import java.util.ArrayList;
+import jakarta.validation.Valid;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import org.example.dto.NewBookDTO;
 import org.example.entity.Book;
+import org.example.services.BookService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("api/v1/books")
 public class BookController {
 
-  @GetMapping("/books")
+  private final BookService bookService;
+
+  public BookController(BookService bookService) {
+    this.bookService = bookService;
+  }
+
+  @GetMapping
   public ResponseEntity<List<Book>> getAllBooks() {
-    AtomicLong id = new AtomicLong(0);
-    var books = new ArrayList<>(List.of(
-        new Book("The Dark Tower", "Steven King"),
-        new Book("The name of the Wind", "Patric Rotfuss"),
-        new Book("A Game of Thrones", "George Martin")
-    ));
-    books.forEach(book -> book.setId(id.incrementAndGet()));
-    return ResponseEntity.ok(books);
+    return ResponseEntity.ok(bookService.findAllBooks());
   }
 
-  @PostMapping("/books")
-  public ResponseEntity<Book> saveBook(@RequestBody BookDTO newBook) {
-    var savedBook = new Book(newBook.title(), newBook.author());
-    savedBook.setId(12345L);
-    return ResponseEntity.ok(savedBook);
+  @PostMapping
+  public ResponseEntity<?> saveBook(@RequestBody @Valid NewBookDTO newBookDTO,
+      BindingResult bindingResult) {
+    if (bindingResult.hasFieldErrors()) {
+      return ResponseEntity.badRequest().
+          body(bindingResult.getFieldErrors().stream().
+              collect(Collectors.groupingBy(
+                  FieldError::getField,
+                  Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
+              )));
+    }
+    var book = new Book(newBookDTO.title(), newBookDTO.author());
+    var savedBook = bookService.addNewBook(book);
+    return ResponseEntity.status(202).body(savedBook);
   }
 
-  public record BookDTO(String title, String author) {
-
+  @GetMapping("{id}")
+  public ResponseEntity<?> getBookById(@PathVariable(name = "id") Long id) {
+    return bookService.findById(id).
+        map(ResponseEntity::ok).
+        orElse(ResponseEntity.notFound().build());
   }
 
 }
