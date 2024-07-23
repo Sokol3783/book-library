cbfbd9727e1aec87ef4cceeb42d880b5c25d8625
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-07-23 22:22:31 +0300

    fix regexp patterns

--- src/main/java/org/example/dto/NewBookDTO.java
+++ src/main/java/org/example/dto/NewBookDTO.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.Size;
 
 public record NewBookDTO(
     @Size(min = 5, max = 100, message = "Invalid length. Title should contain more than 5 chars and less than 100 ones")
-    @Pattern(regexp = "[|/#%=+*_><\\\\]", message = "Title contains invalid symbols: |/\\#%=+*_><]")
+    @Pattern(regexp = "^[^|/#%=+*_><\\\\]*$", message = "Title contains invalid symbols: |/\\#%=+*_><]")
     String title,
     @Size(min = 5, max = 30, message = "Invalid length. Name should contain more than 5 chars and less than 30 ones")
-    @Pattern(regexp = "[^\\w' -]", message = "Name must contain only letters, spaces, dashes, apostrophes!")
+    @Pattern(regexp = "^[\\w' -]+$", message = "Name must contain only letters, spaces, dashes, apostrophes!")
     String author) {
 
 }
